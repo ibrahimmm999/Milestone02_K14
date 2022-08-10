3b9059ac72022a6d6172e642b1b1a990b5c14cbc
Ibrahim Imam <ibrhmmm552@gmail.com>
2022-08-10 14:26:36 +0700

    fixed bugs

--- src/lib/widgets/custom_settingbutton.dart
+++ src/lib/widgets/custom_settingbutton.dart
@@ -7,66 +7,61 @@ class SettingsButton extends StatelessWidget {
   final Function() onPressed;
   final String imagefile;
 
-  const SettingsButton(
-      {Key? key,
-        this.buttonText = '',
-        this.buttonDesc = '',
-        required this.onPressed,
-        required this.imagefile,})
-      : super(key: key);
+  const SettingsButton({
+    Key? key,
+    this.buttonText = '',
+    this.buttonDesc = '',
+    required this.onPressed,
+    required this.imagefile,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 64,
-      child: TextButton(
-          onPressed: onPressed,
-          child: Container(
-            width: double.infinity,
-            alignment: Alignment.topLeft,
-            child: Row(
-              children: [
-                Container(
-                  width: 45,
-                  height: 45,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(imagefile))),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 14),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-
-                      Spacer(),
-
-                      Text(buttonText,
-                        style: blackTextStyle.copyWith(
-                            fontSize: 18,
-                            fontWeight: reguler,
-                          ),),
-
-                      Spacer(),
-
-
-                      Text(buttonDesc,
-                        style: greyTextStyle.copyWith(
-                          fontSize: 16,
-                          fontWeight: light,
-                        ),),
-
-                      Spacer(),
-
-                    ],
-                  ),
-                )
-              ],
-            )
-          ),
-          style: TextButton.styleFrom(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))))
-      );
+        width: double.infinity,
+        height: 67,
+        child: TextButton(
+            onPressed: onPressed,
+            child: Container(
+                width: double.infinity,
+                alignment: Alignment.topLeft,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(image: AssetImage(imagefile))),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 14),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Spacer(),
+                          Text(
+                            buttonText,
+                            style: blackTextStyle.copyWith(
+                              fontSize: 18,
+                              fontWeight: reguler,
+                            ),
+                          ),
+                          Spacer(),
+                          Text(
+                            buttonDesc,
+                            style: greyTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: light,
+                            ),
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+            style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0)))));
   }
 }
